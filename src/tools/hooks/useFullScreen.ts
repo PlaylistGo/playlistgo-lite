799aab3238347871f51a7aeecb40f30f634fb3ff
jasurlive <jasur@graduate.org>
2026-09-23 21:43:19 +0500

    fullscreen hook and dark theme added

---
+++ src/tools/hooks/useFullScreen.ts
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const useFullscreen = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = async () => {
+    if (!document.fullscreenElement) {
+      await document.documentElement.requestFullscreen();
+      setIsFullscreen(true);
+    } else {
+      await document.exitFullscreen();
+      setIsFullscreen(false);
+    }
+  };
+
+  return { isFullscreen, toggleFullscreen };
+};
