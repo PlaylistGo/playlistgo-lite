77d1bf972ff9874fb19a53f11d57ac1f5e1b6111
jasurlive <jasur@graduate.org>
2026-09-23 17:33:59 +0500

    embed youtube link fix

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import "../css/react/App.css";
 import { useCSV } from "../tools/hooks/useCSV";
+import { type Song } from "../tools/types/interface";
+
+function toEmbedYTUrl(url: string) {
+  const u = new URL(url);
+  const id = u.hostname.includes("youtu.be")
+    ? u.pathname.slice(1)
+    : u.searchParams.get("v");
+
+  return id ? `https://www.youtube.com/embed/${id}` : url;
+}
 
 function App() {
   const songs = useCSV();
+  const [activeSong, setActiveSong] = useState<Song | null>(null);
+
   return (
-    <>
+    <ul>
       {songs.map((song) => (
-        <div key={song.id}>
-          <div>{song.title} (Song ID: {song.id})</div>
-          <a href={song.url}>{song.url}</a>
-        </div>
+        <li key={song.id}>
+          <div onClick={() => setActiveSong(song)}>
+            {song.title} (Song ID: {song.id})
+          </div>
+          {activeSong?.id === song.id && (
+            <iframe src={toEmbedYTUrl(song.url)} title={song.title}></iframe>
+          )}
+        </li>
       ))}
-    </>
+    </ul>
   );
 }
 
