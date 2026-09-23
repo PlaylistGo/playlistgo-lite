26b14f7c350e22640e0668c260640a8db55c25aa
jasurlive <jasur@graduate.org>
2026-09-23 22:58:50 +0500

    30% left sidebar

--- src/tools/components/FrameYouTube.tsx
+++ src/tools/components/FrameYouTube.tsx
@@ -3,24 +3,23 @@ import { useState } from "react";
 import { useCSV } from "../hooks/useCSV";
 import { type Song } from "../types/interface";
 import { useURL } from "../hooks/useURL";
+import "../../css/pages/iframe.css"
 
 export function FrameYouTube() {
   const songs = useCSV();
   const [activeSong, setActiveSong] = useState<Song | null>(null);
   return (
-    <>
-      <ul>
+    <div className="iframe-container">
         {songs.map((song) => (
-          <li key={song.id}>
+          <button key={song.id} className="list">
             <div onClick={() => setActiveSong(song)}>
               {song.title} (Song ID: {song.id})
             </div>
             {activeSong?.id === song.id && (
               <iframe src={useURL(song.url)} title={song.title}></iframe>
             )}
-          </li>
+          </button>
         ))}
-      </ul>
-    </>
+    </div>
   );
 }
